3b12e083679830b9cf8dca9a1bcbd7ce4897f44b
renzc <renzc@autoai.com>
2021-04-23 18:49:53 +0800

    Add hash demo

---
+++ tocken/tocken/hash_table.h
@@ -0,0 +1,30 @@
+//
+//  hash_table.h
+//  tocken
+//
+//  Created by tuRen on 2021/4/23.
+//  https://github.com/jamesroutley/write-a-hash-table/tree/master/07-appendix demo
+
+#ifndef hash_table_h
+#define hash_table_h
+
+#include <stdio.h>
+
+typedef struct {
+    char* key;
+    char* value;
+} ht_item;
+
+typedef struct {
+    int size;
+    int base_size;
+    int count;
+    ht_item** items;
+} ht_hash_table;
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
+
+void ht_del_hash_table(ht_hash_table* table);
+#endif /* hash_table_h */
---
+++ tocken/tocken/prime.h
@@ -0,0 +1,16 @@
+//
+//  prime.h
+//  tocken
+//
+//  Created by tuRen on 2021/4/23.
+//
+
+#ifndef prime_h
+#define prime_h
+
+#include <stdio.h>
+
+int is_prime(const int x);
+int next_prime(int x);
+
+#endif /* prime_h */
